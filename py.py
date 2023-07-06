aef83315bcd6649244076e4d0158fa8b3035c9f5
Wolfyret <74528418+Wolfyret@users.noreply.github.com>
2023-07-06 18:29:28 +0200

    Add files via upload

---
+++ py.py
@@ -0,0 +1,43 @@
+from requests_html import HTMLSession
+session = HTMLSession()
+import time
+import configparser
+config = configparser.ConfigParser()
+config.add_section("data")
+prob = '<a class="fc-login-button" href="/login">'
+last='0'
+from discord import Webhook, SyncWebhook
+import aiohttp
+import os.path
+path = 'settings.ini'
+check_file = os.path.isfile(path)
+print(check_file)
+while True:
+    if check_file == True:
+        r = session.get('https://www.indiegala.com/gameplay-giveaway')
+        config.read('settings.ini')
+        data = dict(config.items('data'))
+        print(data)
+        webhook = SyncWebhook.from_url(data['discordhooklink'])
+        if prob in r.text:
+            print('Live')
+            if last!='live':
+                webhook.send("IGFC is live :green_circle:")    
+                last='live'
+        else:
+            print('Not live')
+            if last!='not':
+                webhook.send("IGFC is not live :red_circle:")  
+                last ='not'
+
+        time.sleep(10)
+    else:
+        dchooklink = input('Your discord hook link: ')
+        print('creating settings.ini')
+        config.set("data", "discordhooklink", dchooklink)
+        config.set("data", "version", '0.0.1')
+        with open("settings.ini", 'w') as settings:
+            config.write(settings)
+        print('created, restart the program to continue')
+        break
+        
